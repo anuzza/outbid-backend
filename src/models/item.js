9dc82afbcf806e81ebfc0332292baba03218ae55
Anuja Sharma <asharm12@cs.olemiss.edu>
2022-10-09 16:05:54 -0500

    added json & timestamp

--- src/models/item.js
+++ src/models/item.js
@@ -1,52 +1,58 @@
 const mongoose = require("mongoose");
 
-const itemSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-
-  description: {
-    type: String,
-    required: true,
-  },
-  condition: {
-    type: String,
-    required: true,
-  },
-  starting_amount: {
-    type: Number,
-    required: true,
-  },
-  current_bid: {
-    type: Number,
-    required: true,
-  },
-  open_bid: {
-    type: Boolean,
-    default: false,
-  },
-  sold: {
-    type: Boolean,
-    default: false,
-    required: true,
-  },
-  creator: {
-    type: String,
-    required: true,
-  },
-  winner: {
-    type: String,
-  },
-  images: [
-    {
-      image: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "image",
-      },
-      source_url: {
-        type: String,
-      },
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-});
+
+    description: {
+      type: String,
+      required: true,
+    },
+    condition: {
+      type: String,
+      required: true,
+    },
+    starting_amount: {
+      type: Number,
+      required: true,
+    },
+    current_bid: {
+      type: Number,
+      required: true,
+    },
+    open_bid: {
+      type: Boolean,
+      default: false,
+    },
+    sold: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    creator: {
+      type: String,
+      required: true,
+    },
+    winner: {
+      type: String,
+    },
+    images: [
+      {
+        image: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "image",
+        },
+        source_url: {
+          type: String,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: true,
+    timestamps: true,
+  }
+);
