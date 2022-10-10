1758deb6da72ef99994f66dc412f07ffc526d395
Anuja Sharma <asharm12@cs.olemiss.edu>
2022-10-09 19:37:21 -0500

    signout route added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
+//Todo: Refactor the error msg
 const loginUser = async (req, res) => {
   try {
     const user = await User.findByCredentials(
@@ -13,6 +15,7 @@ const loginUser = async (req, res) => {
   }
 };
 
+//Todo: Refactor the error msg
 const signupUser = async (req, res) => {
   req.body.avatar = "";
   try {
@@ -25,7 +28,34 @@ const signupUser = async (req, res) => {
   }
 };
 
+const logoutUser = async (req, res) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      throw new Error("No token found");
+    }
+    const token = authHeader.replace("Bearer ", "");
+    if (token === "") {
+      throw new Error("No tokens found");
+    }
+    const id = await jwt.decode(token, process.env.JWT_SECRET);
+    const user = await User.findById(id);
+    if (!user) {
+      throw new Error("Not authenticated!");
+    }
+    if (!user.tokens.find(({ token: tok }) => tok === token)) {
+      throw new Error("Invalid token!");
+    }
+    user.tokens = user.tokens.filter(({ token: tok }) => tok !== token);
+    await user.save();
+    res.send();
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
+
 module.exports = {
-  loginUser,
   signupUser,
+  loginUser,
+  logoutUser,
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
-const { signupUser, loginUser } = require("../controllers/userController");
+const {
+  signupUser,
+  loginUser,
+  logoutUser,
+} = require("../controllers/userController");
 
 router.post("/", signupUser);
 router.post("/login", loginUser);
+router.delete("/logout", logoutUser);
 
 module.exports = router;
