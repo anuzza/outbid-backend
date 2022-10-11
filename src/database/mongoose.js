6b68834ead9b30bb798eb9118cc926bad31e496b
jwrigh10 <jwrigh10@cs.olemiss.edu>
2022-10-11 09:15:44 -0500

    finally figured out .env file, thats cool

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://jwright:P4nd4Be4r@cluster0.mv4t0qw.mongodb.net/?retryWrites=true&w=majority", {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
