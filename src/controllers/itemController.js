b75ac490536e12534569c65d0c07df112a66ef52
Anuja Sharma <asharm12@cs.olemiss.edu>
2022-11-10 13:24:46 -0600

    added aws image uploader

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -8,27 +8,44 @@ const deleteFile = require("../utils/deleteFile");
 const fs = require("fs");
 const userItem = require("../models/userItem");
 
-const getItem = async (req, res) => {};
+const getItem = async (req, res) => {
+  try {
+    const items = await Item.find({ active: true });
+    res.status(200).send(items);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
 const addItem = async (req, res) => {
   try {
-    let newItem = new Item(req.body);
+    const { id, ...rest } = req.body;
+    let newItem = new Item({ ...req.body, creator: req.user });
     newItem = await newItem.save();
-    // const userItem = new UserItem({
-    //   user: req.user,
-    //   item: newItem,
-    // });
-    // await userItem.save();
     res.status(200).send(newItem);
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
 };
+
 const updateItem = async (req, res) => {};
+
 const getMyItem = async (req, res) => {};
 const deleteItem = async (req, res) => {};
 const getSavedItem = async (req, res) => {};
-const uplaodImage = async (req, res) => {};
+
+const uplaodImage = async (req, res) => {
+  const item = await Item.findById(req.params.id);
+  if (!item) {
+    return res.status(400).send({
+      err: "No such item found",
+    });
+  }
+  item.images = req.file.location;
+  await item.save();
+  res.send();
+};
+
 const getItembyID = async (req, res) => {};
 
 module.exports = {
